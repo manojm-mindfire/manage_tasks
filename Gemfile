3e22c8737f155f1dff67b61fdc914882a3016826
Manoj Monga <manojm@mindfiresolutions.com>
2012-08-02 15:11:27 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.4'
+gem 'sqlite3'
+gem 'therubyracer'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManageTasks::Application.config.secret_token = '1a47dab5290c0f403a89bde1cc420a497b6baf1c3a972346d1159daae2a5f2c4dd92596912b47f03390fac15ac4b902baa031da4b39daeadc723969b08770783'
